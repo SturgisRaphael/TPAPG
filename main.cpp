8af5071b6448bec682801b4d0a5958542973228d
SturgisRaphael <raphael.sturgis@gmail.com>
2018-04-09 21:26:15 +0200

    c'est la merde

--- main.cpp
+++ main.cpp
@@ -3,8 +3,13 @@
 #include <sstream>
 #include <vector>
 #include <ctime>
+#include <cstring>
 #include "supplier.h"
 #include "service.h"
+
+void affichageXML(string basic_string, service service, int optimum);
+
+void affichageSimple(string nameOfService, service service1, int opti);
 
 using namespace std;
 
@@ -104,18 +109,36 @@ void affichage(string nameOfService, service service1){
 }
 
 int main(int argc, char* argv[]) {
-    ifstream input;
+    ifstream input, opt;
     //input.open( "../Test/BildeKrarup/B/B1.1" );
     //input.open("D:\\Raphael\\Programing\\Git\\TPAPG\\Test\\BildeKrarup\\B\\B1.1");
     //input.open( "../Test/test" );
     //input.open("D:\\Raphael\\Programing\\Git\\TPAPG\\Test\\test");
+
     input.open(argv[1]);
-    string line, nameOfService;
+    opt.open(strcat(argv[1], ".opt"));
+
+    if(!opt){
+        cout << "Unable to open file";
+        exit(1); // terminate with error
+    }
+
+    string line, nameOfService, line2;
     int numberOfSuppliers, numberOfClients, id, costOpening, tmp;
 
     if (!input) {
         cout << "Unable to open file";
         exit(1); // terminate with error
+    }
+
+    getline( opt, line2 );
+
+    istringstream iss(line2);
+    int opti = 0, next = 0;
+    while(iss)
+    {
+        opti = next;
+        iss >> next;
     }
 
     getline( input, line );
@@ -144,7 +167,164 @@ int main(int argc, char* argv[]) {
 
         service1.setSupplier(i, supplier(id, costOpening, costConnexions));
     }
-    affichage(nameOfService, service1);
+    //affichage(nameOfService, service1);
+
+    //affichageXML(nameOfService, service1, opti);
+
+    affichageSimple(nameOfService, service1, opti);
 
     return 0;
 }
+
+void affichageSimple(string nameOfService, service service1, int opti) {
+    clock_t begin;
+    clock_t end;
+    cout  << nameOfService << "," << opti;
+    service1.closeAllSupplier();
+
+    begin = clock();
+
+    service1.Algorithm1();
+
+    end = clock();
+
+    cout << ", " << service1.evaluate() << ", "<< double(end - begin)/ CLOCKS_PER_SEC;
+
+    service1.closeAllSupplier();
+
+    begin = clock();
+
+    service1.Algorithm2(2);
+
+    end = clock();
+
+    cout << ", " << service1.evaluate() << ", "<< double(end - begin)/ CLOCKS_PER_SEC;
+
+    service1.closeAllSupplier();
+
+    begin = clock();
+
+    service1.Algorithm2(1);
+
+    end = clock();
+
+    cout << ", " << service1.evaluate() << ", "<< double(end - begin)/ CLOCKS_PER_SEC;
+
+    service1.closeAllSupplier();
+
+    begin = clock();
+
+    service1.Algorithme3(2);
+
+    end = clock();
+
+    cout << ", " << service1.evaluate() << ", "<< double(end - begin)/ CLOCKS_PER_SEC;
+
+    service1.closeAllSupplier();
+
+    begin = clock();
+
+    service1.Algorithme3(1);
+
+    end = clock();
+
+    cout << "," << service1.evaluate() << ", " << double(end - begin)/ CLOCKS_PER_SEC << endl;
+}
+
+void affichageXML(string nameOfService, service service1, int optimum) {
+    clock_t begin;
+    clock_t end;
+    cout << "<service name='" << nameOfService << "'>\n<optimum>" << optimum << "</optimum>" << endl;
+    service1.closeAllSupplier();
+
+    begin = clock();
+
+    service1.Algorithm1();
+
+    end = clock();
+
+    cout << "<Algo1>\n<value>" << service1.evaluate() << "</value>\n<solution>";
+    for(int i : service1.getO()){
+        if(i != -1)
+            cout << service1.getSupplier(i).getId();
+        else
+            break;
+        cout << ",";
+    }
+    cout << "</solution>\n<time>" << double(end - begin)/ CLOCKS_PER_SEC << "</time>" << endl;
+
+    service1.closeAllSupplier();
+
+    begin = clock();
+
+    service1.Algorithm2(2);
+
+    end = clock();
+
+    cout << "<Algo2 f='2'>\n<value>" << service1.evaluate() << "</value>\n<solution>";
+    for(int i : service1.getO()){
+        if(i != -1)
+            cout << service1.getSupplier(i).getId();
+        else
+            break;
+        cout << ",";
+    }
+    cout << "</solution>\n<time>" << double(end - begin)/ CLOCKS_PER_SEC << "</time>" << endl;
+
+    service1.closeAllSupplier();
+
+    begin = clock();
+
+    service1.Algorithm2(1);
+
+    end = clock();
+
+    cout << "<Algo2 f='1'>\n<value>" << service1.evaluate() << "</value>\n<solution>";
+    for(int i : service1.getO()){
+        if(i != -1)
+            cout << service1.getSupplier(i).getId();
+        else
+            break;
+        cout << ",";
+    }
+    cout << "</solution>\n<time>" << double(end - begin)/ CLOCKS_PER_SEC << "</time>" << endl;
+
+    service1.closeAllSupplier();
+
+    begin = clock();
+
+    service1.Algorithme3(2);
+
+    end = clock();
+
+    cout << "<Algo3 f='2'>\n<value>" << service1.evaluate() << "</value>\n<solution>";
+    for(int i : service1.getO()){
+        if(i != -1)
+            cout << service1.getSupplier(i).getId();
+        else
+            break;
+        cout << ",";
+    }
+    cout << "</solution>\n<time>" << double(end - begin)/ CLOCKS_PER_SEC << "</time>" << endl;
+
+    service1.closeAllSupplier();
+
+    begin = clock();
+
+    service1.Algorithme3(1);
+
+    end = clock();
+
+    cout << "<Algo3 f='1'>\n<value>" << service1.evaluate() << "</value>\n<solution>";
+    for(int i : service1.getO()){
+        if(i != -1)
+            cout << service1.getSupplier(i).getId();
+        else
+            break;
+        cout << ",";
+    }
+    cout << "</solution>\n<time>" << double(end - begin)/ CLOCKS_PER_SEC << "</time>" << endl;
+
+    cout << "</service>" << endl;
+}
+
